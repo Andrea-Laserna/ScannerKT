b3ed8c90e4024c692d3c60627a0235ae166f7013
anderrated <andrealaserna1@gmail.com>
2025-09-02 22:06:00 +0800

    single char, operators, longer lexemes, error implemented

--- main.kt
+++ main.kt
@@ -1,7 +1,84 @@
 import java.util.Scanner
 
-class Tokenization {
+class TokenScanner (val source: String) {
+    // to store Token objects
+    val tokens: MutableList<Token> = mutableListOf()
+    var start = 0
+    var current  = 0
+    var line = 1
 
+    // list of tokens to generate
+    fun scanTokens(): List<Token> {
+        while (!isAtEnd()) {
+            start = current
+            scanToken()
+        }
+
+        tokens.add(Token(EOF, "", null, line))
+        return tokens
+    }
+
+    // tells if all characters have been scanned
+    private fun isAtEnd(): Boolean {
+        return current >= source.length()
+    }
+
+    private fun scanToken() {
+        val c = advance()
+        when (c) {
+            // Single Character
+            '(' -> addToken(LEFT_PAREN)
+            ')' -> addToken(RIGHT_PAREN)
+            '{' -> addToken(LEFT_BRACE)
+            '}' -> addToken(RIGHT_BRACE)
+            ',' -> addToken(COMMA)
+            '.' -> addToken(DOT)
+            '-' -> addToken(MINUS)
+            '+' -> addToken(PLUS)
+            ';' -> addToken(SEMICOLON)
+            '*' -> addToken(STAR)
+            // Operators
+            '!' -> addToken(if (match('=')) BANG_EQUAL else BANG)
+            '=' -> addToken(if (match('=')) EQUAL_EQUAL else EQUAL)
+            '<' -> addToken(if (match('=')) LESS_EQUAL else LESS)
+            '>' -> addToken(if (match('=')) GREATER_EQUAL else GREATER)
+            // Longer Lexemes
+            '/' -> if (match('/')) {
+                while (peek() != '\n' && !isAtEnd()) advance();
+            } else {
+                addToken(SLASH)
+            }
+            // Error
+            else -> throw IllegalArgumentException("Unexpected character: $c at line $line")
+        }
+    }
+
+    // return next character from source string
+    private fun advance(): Char {
+        return source[current++]    
+    }
+
+    private fun addToken(type: TokenType) {
+        addToken(type, null)
+    }
+
+    private fun addToken(TokenType type, Object literal) {
+        val text = source.substring(start, current)
+        tokens.add(new Token(type, text, literal, line))
+    }
+    
+    private fun match(expected: Char): Boolean {
+        if (isAtEnd()) return false
+        if (source[current] != expected) return false
+
+        current++
+        return true
+    }
+    
+    private fun peek(): Char {
+        if (isAtEnd()) return '\u0000'
+        return source[current]
+    }
 }
 
 fun main() {
@@ -10,5 +87,5 @@ fun main() {
 
     val userInput = sc.nextLine()
 
-    // Perform Tokenization on userInput
+    // Perform Scanning on userInput
 }
