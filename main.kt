7b3bc4347c688afbd917268541f8740597d9ce3d
anderrated <andrealaserna1@gmail.com>
2025-09-02 10:59:11 +0800

    initialized main.kt

---
+++ main.kt
@@ -0,0 +1,14 @@
+import java.util.Scanner
+
+class Tokenization {
+
+}
+
+fun main() {
+    val sc = Scanner(System.`in`)
+    println("Enter your code: ")
+
+    val userInput = sc.nextLine()
+
+    // Perform Tokenization on userInput
+}
