be8b2774621bfa511f277ad72cd1a43fb2b4e89c
anderrated <andrealaserna1@gmail.com>
2025-09-03 22:43:22 +0800

    initialized run

--- updated.kt
+++ updated.kt
@@ -4,24 +4,24 @@ import kotlin.system.exitProcess
 
 // entry point
 object Alpha {
-    @JvmStatic // true static method that converts it into jvm bytecode
-    fun main(args: Array<String>) { // args = command line argument
-        // if user types too many arguments i.e. java -jar updated.jar one two three
+    @JvmStatic // static method that converts it into jvm bytecode
+    fun main(args: Array<String>) {
+        // java -jar updated.jar one two three
         if (args.size > 1) {
             println("Usage: alpha [script]")
             exitProcess(64)
-        // if user gives one argument, read the file i.e. java -jar updated.jar code.txt 
+        // java -jar updated.jar code.txt 
         } else if (args.size == 1) {
             runFile(args[0])
-        // if there are no arguments, ask user for input i.e. java -jar updated.jar
+        // java -jar updated.jar
         } else {
             runPrompt()
         }
 
     }
 
+    // read code from another file 
     private fun runFile(path: String) {
-        // read code from another file after running
         val inputStream: InputStream = File(path).inputStream() // open the file for reading: create a file object and open it as a stream of bytes
         val inputString = inputStream.reader().use {it.readText()} // reader() to wrap byte stream for reading, adding .use to close stream after use, it.readText() for read file as a single string
         println(inputString)
@@ -31,7 +31,7 @@ object Alpha {
     private fun runPrompt() {
         while(true){
             print(">")
-            // if enter, set line to the string. if ctrl D EOF, set line to null
+            // if enter, set line to the string. if ctrl D EOF, exit
             val line = readLine() ?: break
             println("You typed: $line")
             // run(line)
@@ -39,6 +39,11 @@ object Alpha {
     }
 
     private fun run(source: String) {
+        // scan the string
+        Scanner scanner = new Scanner(source)
+        // scan for tokens in the string
+        var tokens: MutableList<Token> = scanner.scanTokens()
+        // print tokens for now
         println("Running: $source")
     }
 }
